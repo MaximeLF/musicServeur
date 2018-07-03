a02148cf415a5d1b7d1bb680440622b8928bac80
Le Feuvre Maxime <maxime.le-feuvre@insa-rennes.fr>
2018-07-03 21:31:09 +0200

    let's try something

--- routes/utilities/youtube.js
+++ routes/utilities/youtube.js
@@ -2,12 +2,25 @@ const Ffmpeg = require('fluent-ffmpeg');
 const ytdl = require('ytdl-core');
 const fs = require('fs');
 const omxPlayer=require('node-omxplayer');
+const through2 = require('through2')
 
 let player =omxPlayer();
 
 function play(url){
-    const audio = ytdl(url,{format:"audio"});
-    player.newSource(audio,"local",false,6000);
+    const audio = ytdl(url);
+    const ffmpeg = new Ffmpeg(audio);
+    const stream = through2();
+
+    try {
+        ffmpeg
+            .format('mp3')
+            .pipe(stream);
+
+        player.newSource(stream,"local",false,6000);
+    } catch (e) {
+        throw e;
+    }
+
 
 }
 exports.play=play;
